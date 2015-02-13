adf9c7f1a7f7796983f48eee1d63a4655c7b2a7a
Bashta <erisonve@gmail.com>
2015-02-13 15:49:18 +0200

    Minor Fix

--- Sample/Sample/GameViewController.swift
+++ Sample/Sample/GameViewController.swift
@@ -21,7 +21,7 @@ class GameViewController: UIViewController {
         // create and add a camera to the scene
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
+        scene?.rootNode.addChildNode(cameraNode)
         
         // place the camera
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
@@ -29,22 +29,22 @@ class GameViewController: UIViewController {
         // create and add a light to the scene
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
-        lightNode.light.type = SCNLightTypeOmni
+        lightNode.light?.type = SCNLightTypeOmni
         lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
+        scene?.rootNode.addChildNode(lightNode)
         
         // create and add an ambient light to the scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
-        ambientLightNode.light.type = SCNLightTypeAmbient
-        ambientLightNode.light.color = UIColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
+        ambientLightNode.light?.type = SCNLightTypeAmbient
+        ambientLightNode.light?.color = UIColor.darkGrayColor()
+        scene?.rootNode.addChildNode(ambientLightNode)
         
         // retrieve the ship node
-        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)
+        let ship = scene?.rootNode.childNodeWithName("ship", recursively: true)
         
         // animate the 3d object
-        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+        ship?.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
         
         // retrieve the SCNView
         let scnView = self.view as SCNView
@@ -65,7 +65,7 @@ class GameViewController: UIViewController {
         let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
         let gestureRecognizers = NSMutableArray()
         gestureRecognizers.addObject(tapGesture)
-        gestureRecognizers.addObjectsFromArray(scnView.gestureRecognizers)
+        gestureRecognizers.addObjectsFromArray(scnView.gestureRecognizers!)
         scnView.gestureRecognizers = gestureRecognizers
     }
     
@@ -75,15 +75,15 @@ class GameViewController: UIViewController {
         
         // check what nodes are tapped
         let p = gestureRecognize.locationInView(scnView)
-        let hitResults = scnView.hitTest(p, options: nil)
+        let hitResults = scnView.hitTest(p, options: nil) as Array!
         
         // check that we clicked on at least one object
-        if hitResults.count > 0 {
+        if hitResults?.count > 0 {
             // retrieved the first clicked object
-            let result: AnyObject! = hitResults[0]
+					let result: AnyObject = hitResults[0]
             
             // get its material
-            let material = result.node!.geometry.firstMaterial
+            let material = result.node!.geometry!.firstMaterial
             
             // highlight it
             SCNTransaction.begin()
@@ -94,12 +94,12 @@ class GameViewController: UIViewController {
                 SCNTransaction.begin()
                 SCNTransaction.setAnimationDuration(0.5)
                 
-                material.emission.contents = UIColor.blackColor()
+                material!.emission.contents = UIColor.blackColor()
                 
                 SCNTransaction.commit()
             }
             
-            material.emission.contents = UIColor.redColor()
+            material!.emission.contents = UIColor.redColor()
             
             SCNTransaction.commit()
         }
@@ -111,9 +111,9 @@ class GameViewController: UIViewController {
     
     override func supportedInterfaceOrientations() -> Int {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
         } else {
-            return Int(UIInterfaceOrientationMask.All.toRaw())
+            return Int(UIInterfaceOrientationMask.All.rawValue)
         }
     }
     
